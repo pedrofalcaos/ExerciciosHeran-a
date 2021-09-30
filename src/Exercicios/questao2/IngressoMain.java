5c981547d8988c6589eb82a5f9d43e4461f3ef7f
Pedro Falcão <pedroandre1059@gmail.com>
2021-09-30 00:59:19 -0300

    Primeiro Commit

---
+++ src/Exercicios/questao2/IngressoMain.java
@@ -0,0 +1,19 @@
+package Exercicios.questao2;
+
+import java.util.Scanner;
+
+public class IngressoMain {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor do ingresso:");
+        Ingresso ingresso = new Ingresso();
+        ingresso.setValor(teclado.nextDouble());
+        System.out.println(ingresso);
+        IngressoVip ingressoVip = new IngressoVip();
+        System.out.println("Digite o valor do ingressoVip:");
+        ingressoVip.setValorAdiconal(teclado.nextDouble());
+        System.out.println(ingressoVip);
+
+
+    }
+}
---
+++ src/Exercicios/questao2/IngressoVip.java
@@ -0,0 +1,26 @@
+package Exercicios.questao2;
+
+import java.util.Scanner;
+
+public class IngressoVip extends Ingresso {
+
+    private double valorAdiconal;
+
+    public IngressoVip() {
+        super();
+
+    }
+
+    public double getValorAdiconal() {
+        return valorAdiconal;
+    }
+
+    public void setValorAdiconal(double valorAdiconal) {
+        this.valorAdiconal = valorAdiconal;
+    }
+
+
+    public String toString(){
+
+    }
+}
